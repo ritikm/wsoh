7b425d987dc5d1790bb517825c509ace779fa5b0
Tarun Chaudhry <tarunc92@gmail.com>
2011-07-22 22:47:17 -0700

    app.js

--- app.js
+++ app.js
@@ -86,5 +86,5 @@ app.dynamicHelpers({
     }
 });
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.listen(80);
+console.log('Server listening on port %d in %s mode', app.address().port, app.settings.env);
